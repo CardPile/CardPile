127c7487debcb67e2836a5f181b58886997da2a5
Card Pile <cardpileapp@gmail.com>
2025-05-03 02:39:34 +0200

    Add a bunch of missing files

---
+++ CardPile.Application/Models/CardAnnotationModel.cs
@@ -0,0 +1,18 @@
+﻿using CardPile.Application.Services;
+using ReactiveUI;
+
+namespace CardPile.Application.Models
+{
+    internal class CardAnnotationModel : ReactiveObject, ICardAnnotationService
+    {
+        internal CardAnnotationModel(string name, string textValue)
+        {
+            Name = name;
+            TextValue = textValue;
+        }
+
+        public string Name { get; init; }
+
+        public string TextValue {  get; init; }
+    }
+}
---
+++ CardPile.Application/Views/CardAnnotationView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CardPile.Application.Views;
+
+public partial class CardAnnotationView : UserControl
+{
+    public CardAnnotationView()
+    {
+        InitializeComponent();
+    }
+}
